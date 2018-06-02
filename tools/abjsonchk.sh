2574a44e74ac5fc44f4bed8f808e19790bf3110e
Mark Sellors <github@5vcc.com>
2018-06-02 01:53:02 +0100

    deleted some files not required on this branch

--- tools/abjsonchk.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-cat ./docs/sites.json | jq '' >/dev/null
-if [[ $? -eq 0 ]]; then
-  echo "json validity check - PASS"
-  exit 0
-else
-  echo "json validity check - FAIL"
-  exit 1
-fi
